e842a79d8b94eac7e3f951b40a458147cd1ec3d7
Kieran Matherson <krinto@hotmail.com>
2017-02-20 23:52:37 +1300

    Add register tests

--- test/auth_test.js
+++ test/auth_test.js
@@ -5,7 +5,9 @@ var should = chai.should();
 var expect = chai.expect();
 var chaiHttp = require('chai-http');
 var server = require('../index');
+var mockgoose = require('mockgoose');
 var db = require('./test_helpers');
+var User = require('../models/user');
 
 chai.use(chaiHttp);
 
@@ -29,5 +31,95 @@ describe('Authentication', function() {
                 res.should.have.status(401);
                 done();
             });
+    });
+
+    describe('Register', function() {
+
+        before(function(done) {
+            var newUser = new User({
+                email: 'test@test.com',
+                displayName: 'testing',
+                password: 'password'
+            });
+            newUser.save(function(err, user){
+                if(err){
+                    done(err);
+                }
+            });
+        });
+
+        after(function(done) {
+            mockgoose.reset(function() {
+                done()
+            });
+        });
+
+        it('should fail when no email provided', function(done) {
+            var req = {
+                name: 'bob',
+                password: 'password'
+            };
+            chai.request(server)
+                .post('/api/register')
+                .send(req)
+                .end((err, res) => {
+                    res.should.have.status(422);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('message').eql('You must enter an email');
+                    done();
+            });
+        });
+
+        it('should fail when no password provided', function(done) {
+            var req = {
+                email: 'bob@test.com',
+                name: 'bob'
+            };
+            chai.request(server)
+                .post('/api/register')
+                .send(req)
+                .end((err, res) => {
+                    res.should.have.status(422);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('message').eql('You must enter a password');
+                    done();
+            });
+        });
+
+        it('should fail when user already exists', function(done) {
+            var req = {
+                email: 'test@test.com',
+                displayName: 'testing',
+                password: 'password'
+            };
+            chai.request(server)
+                .post('/api/register')
+                .send(req)
+                .end((err, res) => {
+                    res.should.have.status(422);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('message').eql('Email already in use');
+                    done();
+            });
+        });
+
+        it('should create new user', function(done) {
+            var req = {
+                email: 'bob@test.com',
+                name: 'bob',
+                password: 'password'
+            };
+            chai.request(server)
+                .post('/api/register')
+                .send(req)
+                .end((err, res) => {
+                    res.should.have.status(201);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('message').eql('Successfully created new user');
+                    res.body.should.have.property('token');
+                    res.body.should.have.property('user');
+                    done();
+            });
+        });
     });
 });
